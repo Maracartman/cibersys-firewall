ebf29a905821acb5dbb21d994ead38c585b1a5a9
maracartman <luigi.maracara@gmail.com>
2017-07-19 10:47:35 -0400

    Cambio de nombre de CibersysFirewallAccountsMannager, agregado configuraciòn de JAR ejecutable

---
+++ CibersysFirewallServices/CibersysFirewallAccountsMannager/src/main/java/com/cibersys/firewall/RequestHandlers/PasswordChangeRequestService/PasswordChangeRequest.java
@@ -0,0 +1,11 @@
+package com.cibersys.firewall.RequestHandlers.PasswordChangeRequestService;
+
+
+import com.cibersys.firewall.RequestHandlers.AbstractHandler.AbstractRequestHandlerService;
+import com.cibersys.firewall.domain.models.DTO.ResponseBody.AbstractResponseBody;
+
+/**
+ * Created by Luis Maracara on 6/21/2017.
+ */
+public interface PasswordChangeRequest extends AbstractRequestHandlerService<AbstractResponseBody> {
+}
